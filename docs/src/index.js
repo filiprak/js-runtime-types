c4f60252183fb4d83eb4e906c3f9dd5fcc4c70db
Filip Rak <filip.rak@ikol.com>
2021-01-24 00:26:46 +0100

    Work on type inheritance

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,3 +1,1 @@
-const Types = require('../../src/index');
-
-console.log(Types)
+require('../../src/index');
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const cloneDeep = require('lodash.clonedeep');
 
 const _types = {};
@@ -6,12 +8,15 @@ const __err = (message) => {
     console.error(`JsTypes: ${message}`);
 };
 
-function Base(params) {
-
+function isFunction(val) {
+    return typeof val === 'function';
 }
 
 const JsTypes = {
-    define(name, schema, base = Base) {
+    define(name, schema, Super) {
+
+        Super = Super || this.get('Base') || Object;
+
         if (!name) {
             __err(`Name parameter is empty: ${name}`);
         }
@@ -19,37 +24,62 @@ const JsTypes = {
             __err(`Duplicate type definition: ${name}`);
         }
 
-        function JsRuntimeType(params) {
-            base.call(this, params);
+        function JsTypeConstructor(params) {
+            params = params || {};
 
-            this._state = {};
+            Super.call(this, params);
 
-            for (let key in JsRuntimeType.prototype._schema) {
-                this._state[key] = JsRuntimeType.prototype._schema[key].default || null;
+            Object.defineProperty(this, '__jstype__', {
+                enumerable: true,
+                configurable: true,
+                value: {},
+            });
+
+            const __schema__ = JsTypeConstructor.prototype.__schema__;
+
+            for (let key in __schema__) {
+                if (!isFunction(__schema__[key])) {
+                    if (params[key] !== undefined) {
+                        this.__jstype__[key] = params[key];
+                    } else {
+                        this.__jstype__[key] = __schema__[key].default || null;
+                    }
+                }
             }
         }
 
-        JsRuntimeType.prototype = Object.create(base.prototype);
-        JsRuntimeType.prototype._schema = Object.assign(cloneDeep(base.prototype._schema || {}), cloneDeep(schema));
+        JsTypeConstructor.prototype = Object.create(Super.prototype);
+        JsTypeConstructor.prototype.constructor = JsTypeConstructor;
+
+        Object.defineProperty(JsTypeConstructor.prototype, '__schema__', {
+            enumerable: false,
+            configurable: false,
+            value: Object.assign(cloneDeep(Super.prototype.__schema__ || {}), cloneDeep(schema)),
+        });
 
         for (let key in schema) {
-            console.log('define prop', key);
+            const prop = schema[key];
 
-            Object.defineProperty(JsRuntimeType.prototype, key, {
-                get: function () {
-                    console.log('getter', this);
-                    return this._state[key];
-                },
-                set: function (val) {
-                    console.log('setter', val, this);
-                    this._state[key] = val;
-                },
-            });
+            if (isFunction(prop)) {
+                JsTypeConstructor.prototype[key] = prop;
+
+            } else if (typeof prop === 'object') {
+                Object.defineProperty(JsTypeConstructor.prototype, key, {
+                    enumerable: true,
+                    configurable: true,
+                    get: function () {
+                        return this.__jstype__[key];
+                    },
+                    set: function (val) {
+                        this.__jstype__[key] = val;
+                    },
+                });
+            }
         }
 
-        _types[name] = JsRuntimeType;
+        _types[name] = JsTypeConstructor;
 
-        return JsRuntimeType;
+        return JsTypeConstructor;
     },
     extend(name, base, schema) {
         return this.define(name, schema, this.get(base));
@@ -59,9 +89,25 @@ const JsTypes = {
     },
 };
 
+JsTypes.define('Base', {
+    _base_1: {type: String, default: 111},
+    _base_2: {type: String, default: 222},
+
+    keys: function () {
+        return Object.keys(this.__jstype__ || {});
+    },
+    clone: function () {
+        return cloneDeep(this);
+    },
+});
+
 global.window.Person = JsTypes.define('Person', {
     name: {type: String, default: null},
     surname: {type: String, default: null},
+
+    getFullname: function () {
+        return [this.name, this.surname].join(' ').trim();
+    },
 });
 global.window.User = JsTypes.extend('User', 'Person', {
     id: {type: String, default: null},
